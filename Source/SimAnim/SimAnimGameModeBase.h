ae951ae75d82adebb6b452e2472c1da5e35af36c
SimLapTop\심상우 <simssang2@gmail.com>
2022-02-27 18:12:59 +0900

    애니메이션 블랜딩

---
+++ Source/SimAnim/SimAnimGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimAnimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMANIM_API ASimAnimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
